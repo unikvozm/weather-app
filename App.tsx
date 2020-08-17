d6db8c5412e31e5d8ec190a75a75ebf20feb0e60
Natalia Volkova <Natalia.volkofff@gmail.com>
2020-08-17 22:53:42 +0300

    feat: navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { WeatherScreen } from "./screens/Weather.screen";
+import { DetailsScreen } from "./screens/Details.screen";
+import { RootStackParamList } from "./types/RootStackParamList";
 
+const Stack = createStackNavigator<RootStackParamList>();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#3EEFD8",
+          },
+          headerTintColor: "#121330",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen name="Weather" component={WeatherScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
