1ae8a8ef03db522f7538cd19f1df528928003f21
Natalia Volkova <nataliavolkova@Natalias-MacBook-Pro.local>
2020-08-20 11:22:27 +0300

    integration tests

--- src/__tests__/App-test.tsx
+++ src/__tests__/App-test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render, within } from '@testing-library/react-native';
+import { render, within, fireEvent } from '@testing-library/react-native';
 import Main from '../../index';
 import { getWeatherByCityIds as mockGetWeatherByCityIds } from '../redux/api/weather.api';
-import {data} from '../utils/MockedWeatherData';
+import { data } from '../utils/MockedWeatherData';
 
 // Mock API reposponse
 jest.mock('../redux/api/weather.api');
@@ -20,35 +20,51 @@ test('Fetches and displays list of locations', async () => {
   const items = await listScreen.findAllByA11yHint('Weather Forecast');
   expect(items.length).toEqual(3);
 
-  // General queries for `firstItem` to contain the expected information
   const firstItem = within(items[0]);
-  console.log(firstItem.findAllByText('19.27'))
   expect(firstItem.getByText('London')).toBeTruthy();
   expect(firstItem.getByText('Clouds')).toBeTruthy();
-  // expect(firstItem.getByText('19.27°C')).toBeTruthy();
+  expect(firstItem.getByText('19.27°C')).toBeTruthy();
 
-  // Or more specific queries for `firstItem` to contains the expected information in proper fields
-  // expect(firstItem.getByA11yHint('Location')).toHaveTextContent('Wrocław');
-  // expect(firstItem.getByA11yHint('Weather Description')).toHaveTextContent(
-  //   'Sunny'
-  // );
-  // expect(firstItem.getByA11yHint('Temperature')).toHaveTextContent('25 °C');
+  const secondItem = within(items[1]);
+  expect(secondItem.getByText('Wrocław')).toBeTruthy();
+  expect(secondItem.getByText('Clouds')).toBeTruthy();
+  expect(secondItem.getByText('22.19°C')).toBeTruthy();
 
-  // ... checks for second and third item
+  const thirdItem = within(items[2]);
+  expect(thirdItem.getByText('Berlin')).toBeTruthy();
+  expect(thirdItem.getByText('Clouds')).toBeTruthy();
+  expect(thirdItem.getByText('24.42°C')).toBeTruthy();
+
 });
 
-// test('Navigates to details screen when location is tapped', async () => {
-//   mockGetWeatherData.mockImplementationOnce(() =>
-//     Promise.resolve([wroclaw, newYork, london])
-//   );
+test('Navigates to details screen when location is tapped', async () => {
+  (mockGetWeatherByCityIds as jest.MockedFunction<typeof mockGetWeatherByCityIds>).mockImplementationOnce(() =>
+    Promise.resolve(data)
+  );
 
-//   const app = render(<App />);
+  const app = render(<Main />);
 
-//   const listScreen = within(app.getByA11yHint('Weather List'));
-//   fireEvent.press(await listScreen.findByText('Wrocław'));
+  const listScreen = within(app.getByA11yHint('Weather List'));
+  fireEvent.press(await listScreen.findByText('Wrocław'));
 
-//   const detailsScreen = within(await app.findByA11yHint('Weather Details'));
-//   expect(detailsScreen.getByText('Wrocław')).toBeTruthy();
+  const detailsScreen = within(await app.findByA11yHint('Weather Details'));
+  expect(detailsScreen.getByText('Wrocław')).toBeTruthy();
+  expect(detailsScreen.getByText('Clouds')).toBeTruthy();
+  expect(detailsScreen.getByText('22.19°C')).toBeTruthy();
+  expect(detailsScreen.getByText('Humidity')).toBeTruthy();
+  expect(detailsScreen.getByText('Pressure')).toBeTruthy();
+  expect(detailsScreen.getByText('Wind speed')).toBeTruthy();
+  expect(detailsScreen.getByText('Cloud cover')).toBeTruthy();
 
-//   // ... checks for other weather data
-// });
+  const humidity = within(await app.findByA11yHint('Humidity'));
+  expect(humidity.getByText('73 %')).toBeTruthy();
+
+  const pressure = within(await app.findByA11yHint('Pressure'));
+  expect(pressure.getByText('1010 hPa')).toBeTruthy();
+
+  const windSpeed = within(await app.findByA11yHint('Wind speed'));
+  expect(windSpeed.getByText('2.6 m/s')).toBeTruthy();
+
+  const cloudCover = within(await app.findByA11yHint('Cloud cover'));
+  expect(cloudCover.getByText('40 %')).toBeTruthy();
+});
